c96456f6e73cfb40c8235f0ae525f148802036b4
John Hoder <hoder.john@gmail.com>
2017-07-08 22:59:57 +0200

    Track Pifm as a submodule

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-gcc -std=gnu99 pifm.c -lm
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
 sudo apt-get install omxplayer
