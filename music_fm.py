9d3db70654adacd04bcede2f1ace1d028fc25a39
JohnHoder <hoder.john@gmail.com>
2017-12-02 02:23:47 +0100

    Add internet radio support [nightly]

--- music_fm.py
+++ music_fm.py
@@ -4,7 +4,7 @@ import re
 
 FREQUENCY = "103.3"
 
-def findAndPlay(search):
+def findInYT(search):
 	#Local playing will break FM transmitter
 	#player = subprocess.call(['cvlc',filename,'--play-and-exit'])
 	#player = subprocess.call("omxplayer -o local \"" + filename + "\"", shell=True)
@@ -23,8 +23,17 @@ def findAndPlay(search):
 	yolo = "youtube-dl --verbose --socket-timeout 1000 \"ytsearch:" + search + "\" -f m4a --audio-format m4a -o - | sox -t m4a /dev/stdin -t mp3 /dev/stdout channels 1 | buffer -s 512k -t -m 32M | ffmpeg -v fatal -i pipe:0 -ac 1 -ar 22050 -b 352k -f s16le - | sudo ./pifm/pifm - " + FREQUENCY + "22050"
 	player = subprocess.call(yolo, shell=True)
 
-def urlAndPlay(url):
+def getFromYTurl(url):
 	yolo = "youtube-dl \"" + url + "\" -f m4a --audio-format m4a -o - | sox -t m4a /dev/stdin -t mp3 /dev/stdout channels 1 | buffer -s 512k -t -m 16M | avconv -v fatal -i pipe:0 -ac 1 -ar 22050 -b 352k -f s16le - | sudo ./pifm/pifm - " + FREQUENCY + "22050"
+	player = subprocess.call(yolo, shell=True)
+
+def playFromStream(url):
+	if url == "":
+		file = "listen.pls"
+	else:
+		#TODO: grab url and put it into a new .pls file
+		file = "yolo"
+	yolo = "sox -t mp3 " + file + " -t wav -r 22050 -c 1 - | buffer -s 512k -t -m 16M | avconv -v fatal -i pipe:0 -ac 1 -ar 22050 -b 352k -f s16le - | sudo ./pifm/pifm - " + FREQUENCY + "22050"
 	player = subprocess.call(yolo, shell=True)
 
 if __name__ == '__main__':
@@ -34,10 +43,21 @@ if __name__ == '__main__':
 	elif "--url" in sys.argv:
 		url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', sys.argv[2])[0]
 		print url
-		urlAndPlay(url)
+		getFromYTurl(url)
+		raise SystemExit
+	elif "--radiotest" in sys.argv:
+		playFromStream()
+		raise SystemExit
+	elif "--radio" in sys.argv:
+		if len(sys.argv) == 2:
+			#print "argv len = 2"
+			url = ""
+		else:
+			url = sys.argv[2]
+		playFromStream(url)
 		raise SystemExit
 	else:
-		findAndPlay(sys.argv[1])
+		findInYT(sys.argv[1])
 		raise SystemExit
 
 	if "-h" in sys.argv:
